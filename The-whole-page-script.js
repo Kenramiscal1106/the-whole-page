ec6dbc937bd519cbff9e19ca79e538e56ce41bcb
Ken <ramiscalken@gmail.com>
2021-02-19 07:41:52 +0800

    I don't think this is edited yet

---
+++ The-whole-page-script.js
@@ -0,0 +1,23 @@
+
+function tabs (ui) {
+    
+    var tablinks = document.getElementsByClassName(ui.tabs), tabcontent = document.getElementById(ui.targetElement), i, alltabs = document.getElementsByClassName(ui.tabcontent), icon = document.getElementById('icon')    ;
+    try {
+        icon.href = event.currentTarget.childNodes[0].src;
+        document.title = ui.targetElement;
+    }
+    catch {
+        console.log("no svg")
+    }
+    for (i = 0; i < tablinks.length;i++) {
+        tablinks[i].className = ui.tabs + " none";
+        
+    }
+    for (i = 0; i < alltabs.length;i++) {
+        alltabs[i].style.display = "none"
+    }
+    tabcontent.style.display = "block";
+    //this.className = ui.tabs + ' block';
+    event.currentTarget.className = ui.tabs + " block";
+}
+//syntax: <element onclick="tabs({tabs: all tablinks, targetElement: //the element of target, tabcontent://all targets"></element>
