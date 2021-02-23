aef1750d7ae30fb5af9bdbebe28596586c2d1f02
Ken <ramiscalken@gmail.com>
2021-02-23 10:49:33 +0800

    add chnages to the cookies when live

--- The-whole-page-script.js
+++ The-whole-page-script.js
@@ -1,8 +1,15 @@
 
+/* */
 function tabs (ui) {
+    document.cookie = "lasttab =" + "'" + ui.targetElement + "'";
     var tablinks = document.getElementsByClassName(ui.tabs), tabcontent = document.getElementById(ui.targetElement), i, alltabs = document.getElementsByClassName(ui.tabcontent), icon = document.getElementById('icon')    ;
-    icon.href = event.currentTarget.childNodes[0].src;
-    document.title = ui.targetElement;
+    try {
+        icon.href = event.currentTarget.childNodes[0].src;
+        document.title = ui.targetElement;        
+    }
+    catch {
+        alert("no icon (mandatory)")
+    }
     for (i = 0; i < tablinks.length;i++) {
         tablinks[i].className = ui.tabs + " none";
         
@@ -13,4 +20,18 @@ function tabs (ui) {
     tabcontent.style.display = "block";
     //this.className = ui.tabs + ' block';
     event.currentTarget.className = ui.tabs + " block";
+}
+
+function getcookie(cname) {
+    var name = cname + "=", decodedCookie = decodeURIComponent(document.cookie), ca = decodedCookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while(c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
 }
