1ae8b82fdb44505418312ab356d4397d2c27c7e4
Ken <ramiscalken@gmail.com>
2021-08-24 14:59:39 +0800

    The-whole-page: -renamed them and removed the copy in the end of the file

--- The-whole-page-script.js
+++ The-whole-page-script.js
@@ -1,13 +1,49 @@
-
+function show_more() {
+    var accordion = document.getElementsByClassName('more');
+    for (var i = 0; i < accordion.length;i++) {
+        // accordion[i].className += " block"
+        if (accordion[i].classList[1] == "open") {
+            console.log(2)
+            accordion[i].nextElementSibling.style.height = accordion[i].nextElementSibling.scrollHeight + "px";
+        } else {
+            console.log(1)
+            accordion[i].nextElementSibling.style.height = 0;
+        }
+        accordion[i].addEventListener('click', function() {
+            this.classList.toggle('open');
+            console.log(this.classList)
+            if (this.classList[1] == "open") {
+                this.innerHTML = "<img src='static-icons/close-icon.svg' alt='close'>Show less"
+                this.nextElementSibling.style.height = this.nextElementSibling.scrollHeight + "px";
+            } else {
+                this.innerHTML = "<img src='static-icons/open-icon.svg' alt='close'>Show more"
+                this.nextElementSibling.style.height = 0;
+            }
+        })
+    }
+}
+show_more();
+function side_bar(element, trigger) {
+    var button_trigger = document.querySelector(trigger);
+    var elem = document.querySelector(element.query_selector);
+    button_trigger.addEventListener('click', function() {
+        if (elem.className == (element.default + " enabled")) {
+            elem.className = element.default
+        } else {
+            elem.className = element.default + " enabled"
+        }
+    })
+}
+side_bar({query_selector:".sidebar", default:"sidebar"}, ".tablinks-toggle")
 function tabs (ui) {
     document.cookie = "lasttab =" + "'" + ui.targetElement + "'";
     var tablinks = document.getElementsByClassName(ui.tabs), 
     tabcontent = document.getElementById(ui.targetElement), i, 
     alltabs = document.getElementsByClassName(ui.tabcontent), 
-    icon = document.getElementById('icon')    ;
+    icon = document.getElementById('icon');
     try {
         icon.href = event.currentTarget.childNodes[0].src;
-        document.title = ui.targetElement;        
+        document.title = event.currentTarget.textContent;        
     }
     catch {
         alert("no icon/text (mandatory)")
@@ -36,43 +72,36 @@ function getcookie(cname) {
     }
     return "";
 }
-var themebutton = document.getElementById("settings");
-themebutton.style.transform = "rotate(45deg)"
+var themebutton = document.getElementById("theme-toggle");
+var circle_toggle = themebutton.children[0].children[1];
 function setRaC() {
     if (document.body.className == "") {
-        themebutton.style.transform = "rotate(135deg)"
-        document.body.className = "blue";
-    }
-    else if (document.body.className == "blue") {
-        themebutton.style.transform = "rotate(225deg)"
-        document.body.className = "matrix";
-    }
-    else if (document.body.className == "matrix") {
-        themebutton.style.transform = "rotate(315deg)"
+        themebutton.children[0].style.transform = "rotate(180deg)"
         document.body.className = "dark";
+        themebutton.title="switch to light mode"
+        circle_toggle.style.transform = "translateX(0)" 
     }
     else if (document.body.className == "dark") {
-        themebutton.style.transform = "rotate(45deg)"
+        themebutton.children[0].style.transform = null
         document.body.className = "";
+        themebutton.title="switch to dark mode"
+        circle_toggle.style.transform = "translateX(-15%)"
     }
 }
 themebutton.addEventListener('click', function() {
     setRaC()
     document.cookie = "theme =" + "'" + document.body.className + "'";
-    console.log({
-        classNameOfBody:document.body.className,
-        transform: themebutton.style.transform
-    })
 })
 var lasttheme = getcookie("theme")
 switch(lasttheme) {
     case "dark":
+        themebutton.children[0].style.transform = "rotate(180deg)"
         document.body.className = "dark";
+        circle_toggle.style.transform = "translateX(0)" 
         break;
-    case "matrix":
-        document.body.className = "matrix";
-        break;
-    case "blue":
-        document.body.className = "blue";
+        case "":
+            themebutton.children[0].style.transform = null;
+            document.body.className = "";
+            circle_toggle.style.transform = "translateX(-15%)"
         break;
 }
